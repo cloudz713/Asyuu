b16c0b10e90c77d83ddd89841a84f0cb488e2760
cloudz713 <36653145+cloudz713@users.noreply.github.com>
2024-11-16 18:23:33 +0700

    Create entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+systemctl start mongod.service
+systemctl start genieacs-cwmp
+systemctl start genieacs-nbi
+systemctl start genieacs-fs
+systemctl start genieacs-ui
+echo "Start entrypoint.sh"
+
+set -e
+
+# Folder for sshd. No Change.
+mkdir -p /var/run/sshd
+
+# Key generation
+ls /etc/ssh/ssh_host_* >/dev/null 2>&1 &&echo "Keys is found" ||echo "Key generation." && ssh-keygen -A
+
+# Environment variables that are used if not empty:
+# USER_PASSWORD
+
+#Set password
+if [ -f /.ispasswordset ]; then
+    echo "Password already set"
+else
+    echo "Set  password of user for sshd"
+    echo 'root:'${USER_PASSWORD} |chpasswd
+    touch /.ispasswordset
+fi
+
+echo "Run sshd"
+
+exec "$@"
